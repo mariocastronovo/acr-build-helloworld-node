731a653daa7ce8ca084f8016a582b079422d004f
mariocastronovo <mariocastronovo@hotmail.com>
2022-11-09 17:00:59 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,17 @@ function error {
     echo "Error; $1"
 }
 
+OPTIONS="${RUNNER_OPTIONS:-""}"
+
+GROUP=${RUNNER_GROUP:-"default"}
+
 echo "Configuring GitHub Actions Runner and registering"
 ./config.sh \
     --unattended \
     --url "https://github.com/mariocastronovo/acr-build-helloworld-node" \
     --token "ABZU3MR4NPEOPOQSVI6PDOLDNPAW4" \
     --name "mcstestrunner" \
+    --runnergroup ${GROUP} \
     $OPTIONS
 
 echo "Starting GitHub Actions Runner"
