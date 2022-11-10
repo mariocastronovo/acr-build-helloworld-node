d4a3067295b582b25bd6d9a9038f3ae8104a6e02
mariocastronovo <mariocastronovo@hotmail.com>
2022-11-10 12:11:13 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ echo "Configuring GitHub Actions Runner and registering"
 ./config.sh \
     --unattended \
     --url "https://github.com/mariocastronovo/acr-build-helloworld-node" \
-    --token "ABZU3MXKVDIHW34IZZIS7FTDNS5V6" \
+    --token "ABZU3MSJP6PVJTKGNQ3SZPLDNTVEY" \
     --name "mcstestrunnernew" \
     --runnergroup ${GROUP} \
     $OPTIONS
